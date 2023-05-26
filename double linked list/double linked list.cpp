9726ff9f4675845b32e4ce118efe9a82283fe078
namira121 <127211008+namira121@users.noreply.github.com>
2023-05-26 19:17:09 +0700

    membuat method hapus diluar class DoubleLinkedList

--- double linked list/double linked list.cpp
+++ double linked list/double linked list.cpp
@@ -120,6 +120,24 @@ void DoubleLinkedList::traverse() {
 
 void DoubleLinkedList::revtraverse() {
 	if (ListEmpty()) {
-		cout << "\nList is empty"
+		cout << "\nList is empty" << endl;
+	}
+
+	else {
+		cout << "\nRecords in descending order of roll number are: " << endl;
+		Node* currentNode = START;
+		while (currentNode->next != NULL)
+			currentNode = currentNode->next;
+		
+		while (currentNode != NULL) {
+			cout << currentNode->noMhs << " " << currentNode->name << endl;
+			currentNode = currentNode->prev;
+		}
+	}
+}
+
+void DoubleLinkedList::hapus() {
+	if (ListEmpty()) {
+
 	}
 }
